ffe0e5bd2cc5b1da41ef52e02ad5a7a2dc1a0efb
akash kushwaha <kushwahavijay1893@gamil.com>
2025-12-16 18:58:06 +0530

    database connection

---
+++ src/db/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { MONGO_NAME } from "../constant.js";
+
+
+
+const connectDB = async () => {
+    try {
+        const connectionInstant = await mongoose.connect(`${process.env.MONGODB_URI}/${MONGO_NAME}`)
+        console.log(connectionInstant.connection.host);
+
+
+    } catch (error) {
+        console.log("error in connection of mongodb", error);
+    }
+}
+
+export default connectDB;
